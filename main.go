9f81482f89b6a0a45c56a5d7fa7ecbc184d01951
Kristenn Quemener <kristennquemener@Kristenns-MacBook-Air.attlocal.net>
2024-12-27 18:49:34 -0600

    feat: db

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/KrisQ/go-feed/internal/config"
+	"github.com/KrisQ/go-feed/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -18,7 +22,15 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error connecting to db: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
@@ -26,6 +38,9 @@ func main() {
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerListUsers)
 
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: cli <command> [args...]")
--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE users(
-    id          UUID,
+    id          UUID PRIMARY KEY NOT NULL,
     created_at  TIMESTAMP NOT NULL,
     updated_at  TIMESTAMP NOT NULL,
     name        VARCHAR(255) NOT NULL UNIQUE
